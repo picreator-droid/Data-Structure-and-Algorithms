40a87be66f10e150cddf2515052f3575a0a4fac9
picreator-droid <piyush713713@gmail.com>
2026-08-02 21:19:10 +0530

    Time: 1 ms (99.96%) | Memory: 46.7 MB (83.19%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int single = nums[0];
-        if (nums.length == 1) return single;
+        
         for(int i=1; i<nums.length; i++){
             single = single ^ nums[i];
         }
