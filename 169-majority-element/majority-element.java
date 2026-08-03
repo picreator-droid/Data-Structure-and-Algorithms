6de8e475b8c235f7bfe1c808f2dba82e9cf5689d
picreator-droid <piyush713713@gmail.com>
2026-08-03 22:02:52 +0530

    Time: 1 ms (99.88%) | Memory: 55.6 MB (46.83%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,21 +2,40 @@ class Solution {
     public int majorityElement(int[] nums) {
         
         // brute force:
-        int n = nums.length;
+        // int n = nums.length;
         
-        for(int i=0; i<n; i++){
-            int count = 0;
+        // for(int i=0; i<n; i++){
+        //     int count = 0;
 
-            for(int j=0; j<n; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+        //     for(int j=0; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        // moore's voting algorithm
+        
+        int n=nums.length;
+        int ele=0;
+        int count=0;
+
+        for (int i=0; i<n; i++){
+            if(count==0){
+                count++;
+                ele=nums[i];
             }
-            if(count > n/2){
-                return nums[i];
+            else if(ele==nums[i]){
+                count++;
             }
-        }
-        return -1;
+            else{
+                count--;
+            }
+        }return ele;       
     }
     
 }
