b4ffdc76f326762820130131cbd7a75577a7bf70
picreator-droid <piyush713713@gmail.com>
2026-08-05 01:10:16 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (44.11%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -2,20 +2,43 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
 // Squaring all the elemnets >>  Time complexity : O(n)
 
-        for(int i=0; i<nums.length; i++){
-            nums[i] *= nums[i]; 
-        }
+        // for(int i=0; i<nums.length; i++){
+        //     nums[i] *= nums[i]; 
+        // }
 
 
 //  Arrays.sort(nums); >> Time complexity : O(nlogn)
 
 
-        Arrays.sort(nums);    
-        return nums;
+    //     Arrays.sort(nums);    
+    //     return nums;
+
+    // overall Time Complexity : O(nlogn)
+    
+
+        // Use two pointers to generate the sorted squares in O(n) time.
+
+        int n=nums.length;
+        int[] res=new int[n];
+        for(int i=0; i<n; i++){
+            nums[i] *= nums[i];
+        }
+        int head=0;
+        int tail=n-1;
+        for(int i=n-1; i>=0; i--){
+            if(nums[head]>nums[tail]){
+                res[i]=nums[head];
+                head++;
+            }
+            else{
+                res[i]=nums[tail];
+                tail--;
+            }
+        }
+        return res;
     }
 }
-// overall Time Complexity : O(nlogn)
 
 
-// Use two pointers to generate the sorted squares in O(n) time.
+
 
