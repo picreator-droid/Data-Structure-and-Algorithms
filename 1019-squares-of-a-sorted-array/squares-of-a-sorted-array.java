4a86fd76e2bccdbe18a77cf0358da358eea6a2fd
picreator-droid <piyush713713@gmail.com>
2026-08-05 01:23:54 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (53.90%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -20,18 +20,28 @@ class Solution {
 
         int n=nums.length;
         int[] res=new int[n];
-        for(int i=0; i<n; i++){
-            nums[i] *= nums[i];
-        }
+        // for(int i=0; i<n; i++){
+        //     nums[i] *= nums[i];
+        // }
         int head=0;
         int tail=n-1;
         for(int i=n-1; i>=0; i--){
-            if(nums[head]>nums[tail]){
-                res[i]=nums[head];
+
+            // if(nums[head]>nums[tail]){
+            //     res[i]=nums[head];
+            //     head++;
+            // }
+            // else{
+            //     res[i]=nums[tail];
+            //     tail--;
+            // }
+
+            if(Math.abs(nums[head])>Math.abs(nums[tail])){
+                res[i]=nums[head]*nums[head];
                 head++;
             }
             else{
-                res[i]=nums[tail];
+                res[i]=nums[tail]*nums[tail];
                 tail--;
             }
         }
