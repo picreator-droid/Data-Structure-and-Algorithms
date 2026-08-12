9e09d4b25ba18e229450f6f54dd28db76cdbbab0
picreator-droid <piyush713713@gmail.com>
2026-08-12 22:10:58 +0530

    Time: 20 ms (38.29%) | Memory: 112.5 MB (5.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // for(int i=0; i<nums.length; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
